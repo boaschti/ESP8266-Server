fe6f5b50a1c5732603768cf6151ee48192700069
Basti <sebastian-weisser@gmx.de>
2020-02-21 09:58:52 +0100

    add smaler example to init server

--- ESP_Server/ESP_Server.ino
+++ ESP_Server/ESP_Server.ino
@@ -1,4 +1,12 @@
-#include "ESPServer.h"
+
+
+#define userpage_existing
+//#define AllowAcessPoint
+
+#include "C:\Users\Basti\Documents\ESP8266-Server\ESP_Server\ESPServer.h"
+#include "C:\Users\Basti\Documents\ESP8266-Server\ESP_Server\ESPServer.cpp"
+#include "C:\Users\Basti\Documents\ESP8266-Server\ESP_Server\ESP8266HTTPUpdateServer.h"
+#include "C:\Users\Basti\Documents\ESP8266-Server\ESP_Server\ESP8266HTTPUpdateServer.cpp"
 
   
 
@@ -20,31 +28,22 @@ void MQTTBrokerChanged(){
 */
 
 
-#define userpage_existing
     static const char PROGMEM USERPAGE_HTML[] = R"rawliteral(
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <meta name = "viewport" content = "width = device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable=0">
-      <title>DeviceName MQTT Configuration</title>
-      <style>
-        "body { background-color: #808080; font-family: Arial, Helvetica, Sans-Serif; Color: #000000; }"
-      </style>
-    </head>
-    <body>
-      <h3>Hi Userpage is here!</h3>
-      <a href="/"><button type="button">Home</button></a>
-    </body>
-    </html>
-    )rawliteral";
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name = 'viewport' content = 'width = device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable=0'>
+  <title>RFM69 Node Configuration</title>
+  <style>
+    'body { background-color: #808080; font-family: Arial, Helvetica, Sans-Serif; Color: #000000; }'
+  </style>
+</head>
+<body>
 
-    void handleconfigureUserWrite(){
-        handleRoot();
-    }
-    void handleconfigureUser(){
-        webServersend_P(USERPAGE_HTML);
-    }
-
+    <p><a href="/"><button type="button">Home</button></a>
+</body>
+</html>
+)rawliteral";
 
 void handleconfigureUserWrite(){
     handleRoot();
@@ -65,7 +64,5 @@ void setup(){
 }
 
 void loop(){
-  
      loop_server();
-  
 }
